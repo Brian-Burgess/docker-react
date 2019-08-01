2c06103de4410102e5329aaca99b22b9e34ebcc6
Brian-Burgess <brian@brian-burgess.me>
2019-08-01 10:58:37 -0400

    changed text

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload. Hi Brian!
+          To get started, edit <code>src/App.js</code> and save to reload. Changed on feature.
         </p>
       </div>
     );
